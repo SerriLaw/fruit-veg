281581c9ce363a951c73b194b84bb576d4008bb1
Sarah Lawrence <s.j.lawrence8@gmail.com>
2019-10-20 18:27:53 +1100

    Add optional theming for Block

--- src/components/Block/index.tsx
+++ src/components/Block/index.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import { Pane } from 'evergreen-ui';
 
+import withTheme from '../../HOCs/themed';
+
 interface Props {
   children?: any;
   [x: string]: any;
 }
 
-export default (props: Props) => {
-  const { children, ...styling } = props;
-  return <Pane {...styling}>{children}</Pane>;
-};
+interface BlockWithThemeProps extends Props {
+  themeKey: string;
+}
+
+export default class Block extends React.Component<Props> {
+  render() {
+    const { children, ...styling } = this.props;
+    return <Pane {...styling}>{children}</Pane>;
+  }
+}
+
+export const BlockWithTheme = (props: BlockWithThemeProps) =>
+  withTheme(Block, props);
